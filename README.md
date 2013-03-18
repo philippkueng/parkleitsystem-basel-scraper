be97530481a6415692c52851b6d26c8a09c0075d
Philipp Küng <git@philippkueng.ch>
2013-03-18 23:00:48 +0100

    initial version without Google Docs support

---
+++ README.md
@@ -0,0 +1,13 @@
+# Parkleitsystem Basel Scraper
+
+## Setup (Development)
+
+1. Install python, pip and virtualenv
+2. clone the repository
+3. `virtualenv venv --no-site-packages`
+4. source venv/bin/activate
+5. pip install -r requirements.txt
+
+## Setup (deploy on heroku)
+
+1. read https://devcenter.heroku.com/articles/python
---
+++ app.py
@@ -0,0 +1,80 @@
+import os
+from flask import Flask
+from flask.ext.heroku import Heroku
+import gdata.docs.service
+import urllib3
+import lxml.html
+from datetime import datetime
+
+app = Flask(__name__)
+heroku = Heroku(app)
+
+app.config['DEBUG'] = os.environ.get('DEBUG')
+
+@app.route('/')
+def root():
+    return 'Welcome to the Parkleitsytem Basel Scraper'
+
+
+@app.route('/scrape')
+def scrape():
+    app.logger.info('start processing...')
+    timestamp = datetime.now()
+    timestamp_str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+    # fetch the html document from the remote website
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'http://www.parkleitsystem-basel.ch/stadtplan.php')
+    if r.status == 200: # response was correct, continue with the parsing process
+
+        app.logger.info('fetched html successfully, parsing now...')
+        
+        # parse the html response
+        root = lxml.html.fromstring(r.data)
+
+        # collect measurements
+        lots = []
+
+        # go through the document and extract each parking name and available lot count
+        for tr in root.cssselect("table[summary='Parkhaus DATEN']>tr[class='boxstatustr']"):
+            parkingarea_name = tr.cssselect('td>a')[0].text
+            parkingarea_lots = tr.cssselect('td>b')[0].text
+            lots.append(parkingarea_lots)
+            app.logger.info(parkingarea_name + ' has ' + parkingarea_lots + ' available at lots ' + timestamp_str)
+
+
+        # Create a client class which will make HTTP requests with Google Docs server.
+        # client = gdata.docs.service.DocsService()
+        # Authenticate using your Google Docs email address and password.
+        # client.ClientLogin(os.environ.get('USERNAME'), os.environ.get('PASSWORD'))
+
+        # make sure the Google Spreadsheet exists
+
+        # os.environ('SPREADSHEET_NAME')
+
+        # insert another line into the Spreadsheet
+
+
+    else: # response was invalid, the site might be down or there's a network error
+        # go check again! // TODO
+        app.logger.info('the site might be down, check again in a couple of minutes')
+        app.logger.error('http status code: ' + r.status)
+
+
+    # send the data to the Google Spreadsheet
+
+
+
+    # # # Query the server for an Atom feed containing a list of your documents.
+    # documents_feed = client.GetDocumentListFeed()
+    # # Loop through the feed and extract each document entry.
+    # for document_entry in documents_feed.entry:
+    #   # Display the title of the document on the command line.
+    #   app.logger.info(document_entry.title.text)
+
+    return "Website scraped on " + timestamp_str
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
