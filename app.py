0d1972f952c8828b51a398725f209fde984240d6
Philipp Küng <git@philippkueng.ch>
2013-05-25 18:39:42 +0200

    importing sys; replaced self with sys

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import gdata.spreadsheet.service
 import gdata.service
 import gdata.spreadsheet
@@ -7,7 +8,7 @@ import lxml.html
 from datetime import datetime
 
 def scrape():
-    self.stdout.write('start processing...')
+    sys.stdout.write('start processing...')
     timestamp = datetime.now()
     timestamp_str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -16,7 +17,7 @@ def scrape():
     r = http.request('GET', 'http://www.parkleitsystem-basel.ch/stadtplan.php')
     if r.status == 200: # response was correct, continue with the parsing process
 
-        self.stdout.write('fetched html successfully, parsing now...')
+        sys.stdout.write('fetched html successfully, parsing now...')
         
         # parse the html response
         root = lxml.html.fromstring(r.data)
@@ -29,7 +30,7 @@ def scrape():
             parkingarea_name = tr.cssselect('td>a')[0].text
             parkingarea_lots = tr.cssselect('td>b')[0].text
             lots.append(parkingarea_lots)
-            self.stdout.write(parkingarea_name + ' has ' + parkingarea_lots + ' available at lots ' + timestamp_str)
+            sys.stdout.write(parkingarea_name + ' has ' + parkingarea_lots + ' available at lots ' + timestamp_str)
 
 
         # Create a client class which will make HTTP requests with Google Spreadsheets server.
@@ -62,15 +63,15 @@ def scrape():
         # insert the data into the Google Spreadsheet
         entry = client.InsertRow(entry_line, os.environ.get('SPREADSHEET_KEY'), os.environ.get('WORKSHEET_ID'))
         if isinstance(entry, gdata.spreadsheet.SpreadsheetsList):
-            self.stdout.write('scraping for ' + timestamp_str + ' was successful.')
+            sys.stdout.write('scraping for ' + timestamp_str + ' was successful.')
         else:
-            self.stdout.write('inserting an entry failed')
+            sys.stdout.write('inserting an entry failed')
 
 
     else: # response was invalid, the site might be down or there's a network error
         # go check again! // TODO
-        self.stdout.write('the site might be down, check again in a couple of minutes')
-        self.stdout.write('http status code: ' + r.status)
+        sys.stdout.write('the site might be down, check again in a couple of minutes')
+        sys.stdout.write('http status code: ' + r.status)
 
 
 
